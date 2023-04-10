9236231bef08848e27b41b81d5e102190e597fa0
SK2091Momys <Spirichev_llirik@mail.ru>
2023-04-10 21:05:13 +0300

    DEV-PY100.S23.Credits.Game_XO.v1.2 c внесенными исправлениями

--- main.py
+++ main.py
@@ -7,56 +7,66 @@ def game():
     WIN_1 = 0
     WIN_2 = 0
 
-    def made_field(n):
+    def made_field(n: int) -> list:
         """
-        Функция создающая игровое поле заданного размерами N на N
-        :return: игровое поле типа список c количеством элементов N ** 2
+        Функция создающая игровое поле заданного размерами N на N.
+        :param n: целое число, принимает значение необходимого размера поля.
+        :return:  elem: игровое поле типа список c количеством элементов N в квадрате.
         """
-        elem = [["*" for _ in range(1, n + 1)] for _ in range(1, n + 1)]
+        elem = [["*" for _ in range(n)] for _ in range(n)]
         return elem
 
-    def draw_field(field):
+    def draw_field(field: list):
         """
-        Функция позволяющая вывести игровое поле в виде матрицы
+        Функция позволяющая вывести игровое поле в виде матрицы.
         :return:
         """
         for _ in field:
             print(f"{_}")
 
-    def motion_p1(field):
+    def motion(field: list, player: str) -> list:
         """
-        Функция хода произведенного первым игроком.
-        :return: Возврощает список игрового поля с записанным ходом первого игрока
+        Функция хода произведенного одним из игроков.
+        :param field: список, образующий игровое поле.
+        :param player: переменная, определяющая игрока чей ход осуществляется.
+        :return: возвращает список, образующий игровое поле, с записанным ходом игрока.
         """
-        print(f"Ходит игрок: {PLAYER_1.capitalize()}")
+        print(f"Ходит игрок: {player.capitalize()}")
         while True:
             try:
                 draw_field(FIELD)
                 line = int(input("Введите номер строки необходимой ячейки:\n"))
                 post = int(input("Введите номер столбца необходимой ячейки:\n"))
                 if field[line][post] == "*":
-                    field[line][post] = "X"
+                    if player == PLAYER_1:
+                        field[line][post] = "X"
+                    else:
+                        field[line][post] = "O"
                     break
                 else:
                     print(f"Данная ячейка уже занята, выберите свободную ячейку отмечанную символом *!")
                     continue
             except IndexError:
                 print(f"Такой ячейки не существует! Вводите номера от 0 до {N - 1}!")
+            except ValueError:
+                print(f"Такой ячейки не существует! Вводите номера от 0 до {N - 1}!")
         return field
 
-    def test_win_p1(test_field, n):
+    def test_win(test_field: list, n: int) -> int:
         """
-        Функция проверки уловия победы первого игрока
-        :param test_field: список содержащи игровое поле
+        Функция проверки уловия победы игрока
+        :param test_field: список, содержащи игровое поле
         :param n: размер строки игрового поля
-        :return: возвращает единицу в случае победы или ноль в случае отсутствия таковой.
+        :return: win: возвращает единицу в случае победы или ноль в случае отсутствия таковой.
         """
-        win = 6
+
+        win = 0
         a = [row[idx] for idx, row in enumerate(test_field)]
         # Cписок составленный из элементов диагонали "\" игрового поля
         b = [row[(n - 1) - idx] for idx, row in enumerate(test_field)]
         # Cписок составленный из элементов диагонали "/" игрового поля
         c = []
+        d = []
         # Проверка победы по горизанталям и диагоналям:
         for k in test_field:
             if k.count("X") == n or a.count("X") == n or b.count("X") == n:
@@ -64,70 +74,24 @@ def game():
                 print(f"!!!Поздравляю!!!\nПобедил игрок {PLAYER_1.capitalize()}!")
                 win = 1
                 break
-            else:
-                win = 0
+            elif k.count("O") == n or a.count("O") == n or b.count("O") == n:
+                draw_field(FIELD)
+                print(f"!!!Поздравляю!!!\nПобедил игрок {PLAYER_2.capitalize()}!")
+                win = 1
+                break
         # Проверка победы по вертикалям:
         for k in range(0, n):
             for j in range(0, n - 1):
                 if test_field[j][k] == test_field[abs(j - 1)][k] == test_field[j + 1][k] == "X":
                     c.append("X")
+                elif test_field[j][k] == test_field[abs(j - 1)][k] == test_field[j + 1][k] == "O":
+                    d.append("O")
             if c.count("X") == n - 1:
                 draw_field(FIELD)
                 print(f"!!!Поздравляю!!!\nПобедил игрок {PLAYER_1.capitalize()}!")
                 win = 1
                 break
-        return win
-
-    def motion_p2(field):
-        """
-        Функция хода произведенного вторым игроком.
-        :return: Возврощает список игрового поля с записанным ходом вторго игрока
-        """
-        print(f"Ходит игрок: {PLAYER_2.capitalize()}")
-        while True:
-            try:
-                draw_field(FIELD)
-                line = int(input("Введите номер строки необходимой ячейки:\n"))
-                post = int(input("Введите номер столбца необходимой ячейки:\n"))
-                if field[line][post] == "*":
-                    field[line][post] = "O"
-                    break
-                else:
-                    print(f"Данная ячейка уже занята, выберите свободную ячейку отмечанную символом *!")
-                    continue
-            except IndexError:
-                print(f"Такой ячейки не существует! Вводите номера от 0 до {N - 1}!")
-        return field
-
-    def test_win_p2(test_field, n):
-        """
-        Функция проверки уловия победы первого игрока
-        :param test_field: список содержащи игровое поле
-        :param n: размер строки игрового поля
-        :return: возвращает единицу в случае победы или ноль в случае отсутствия таковой.
-        """
-        win = 7
-        # Cписок составленный из элементов диагонали "\" игрового поля:
-        a = [row[idx] for idx, row in enumerate(test_field)]
-        # Cписок составленный из элементов диагонали "/" игрового поля:
-        b = [row[(n - 1) - idx] for idx, row in enumerate(test_field)]
-        # Cписок составленный из элементов вертикали игрового поля:
-        c = []
-        # Проверка победы по горизанталям и диагоналям:
-        for k in test_field:
-            if k.count("O") == n or a.count("O") == n or b.count("O") == n:
-                draw_field(FIELD)
-                print(f"!!!Поздравляю!!!\nПобедил игрок {PLAYER_2.capitalize()}!")
-                win = 1
-                break
-            else:
-                win = 0
-        # Проверка победы по вертикалям:
-        for k in range(0, n):
-            for j in range(0, n - 1):
-                if test_field[j][k] == test_field[abs(j - 1)][k] == test_field[j + 1][k] == "O":
-                    c.append("O")
-            if c.count("O") == n - 1:
+            elif d.count("O") == n - 1:
                 draw_field(FIELD)
                 print(f"!!!Поздравляю!!!\nПобедил игрок {PLAYER_2.capitalize()}!")
                 win = 1
@@ -161,12 +125,12 @@ def game():
 
     # Игра:
     while WIN_1 == 0 and WIN_2 == 0:
-        motion_p1(FIELD)
-        WIN_1 = test_win_p1(FIELD, N)
+        motion(FIELD, PLAYER_1)
+        WIN_1 = test_win(FIELD, N)
         count += 1
         if WIN_1 != 1 and count < (N ** 2):
-            motion_p2(FIELD)
-            WIN_2 = test_win_p2(FIELD, N)
+            motion(FIELD, PLAYER_2)
+            WIN_2 = test_win(FIELD, N)
             count += 1
         elif WIN_1 != 1 and WIN_2 != 1 and count >= (N ** 2):
             draw_field(FIELD)
